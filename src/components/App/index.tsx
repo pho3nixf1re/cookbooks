ff47fad0aa118d8d0c51cf1a515e64ee216f73b8
Matthew Turney <matthew.a.turney@gmail.com>
2020-10-08 10:26:57 -0500

    update typescript and eslint parser and add rule for import ordering

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
 import { LoginButton } from '../LoginButton'
 import { LogoutButton } from '../LogoutButton'
-import { useAuth0 } from '@auth0/auth0-react'
 import { Profile } from '../Profile'
 import styles from './App.module.scss'
 
