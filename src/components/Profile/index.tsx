30f6fb713d26fba48e13ab7cf5e7513009e6dc1f
Matthew Turney <matthew.a.turney@gmail.com>
2020-10-09 20:56:38 -0500

    implement basic authentication and display the logged-in user

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,5 +1,5 @@
-import React, { Fragment } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
+import React, { Fragment } from 'react'
 
 export const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0()
--- src/hoc/with-auth-provider.tsx
+++ src/hoc/with-auth-provider.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
 import { Auth0Provider } from '@auth0/auth0-react'
+import React from 'react'
 
 export const withAuthProvider = () => <P extends Record<string, unknown>>(
   Component: React.ComponentType<P>
